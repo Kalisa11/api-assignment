70940eb891c1b655f80461aac9eadbe0a64a98fc
honorekalisa <h.kalisa@alustudent.com>
2023-08-18 11:22:36 +0200

    displaying data

--- app/components/Weather.tsx
+++ app/components/Weather.tsx
@@ -15,16 +15,18 @@ const Weather = ({ data }: { data: WeatherData }) => {
           />
           <p className="text-2xl">{data.weather[0].main}</p>
         </div>
-        <p className="text-9xl">{data.main.temp.toFixed(0)}&#176;</p>
+        <p className="text-9xl">{data.main.temp.toFixed(0)}&#176;C</p>
       </div>
 
       <div className="bg-black/50 relative p-8 rounded-md">
-        <p className="text-2xl text-center pb-6">Weather in {data.name}, {data.sys.country}</p>
+        <p className="text-2xl text-center pb-6">
+          Weather in {data.name}, {data.sys.country}
+        </p>
         <div className="flex justify-between text-center">
           <div className="flex flex-col items-center justify-center">
             <WiHorizonAlt size={40} />
             <p className="font-bold text-2xl">
-              {data.main.feels_like.toFixed(0)}&#176;
+              {data.main.feels_like.toFixed(0)}&#176;C
             </p>
             <p className="text-xl">Feels Like</p>
           </div>
@@ -36,9 +38,27 @@ const Weather = ({ data }: { data: WeatherData }) => {
           <div className="flex flex-col items-center justify-center">
             <WiStrongWind size={40} />
             <p className="font-bold text-2xl">
-              {data.wind.speed.toFixed(0)} KPH
+              {data.wind.speed.toFixed(0)} m/s
             </p>
-            <p className="text-xl">Winds</p>
+            <p className="text-xl">Wind</p>
+          </div>
+        </div>
+        <div className="flex pt-5 justify-between">
+          <div>
+            <p>
+              <strong>Min:</strong> {data.main.temp_min}°C
+            </p>
+            <p>
+              <strong>Max:</strong> {data.main.temp_max}°C
+            </p>
+          </div>
+          <div>
+            <p>
+              Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
+            </p>
+            <p>
+              Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
+            </p>
           </div>
         </div>
       </div>
